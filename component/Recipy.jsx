b00f2bdb67fe495404930b4c24931cca8d23b1fe
Gissell10 <gissellbustosb@gmail.com>
2023-08-21 15:02:35 +1000

    fixing recipe image

--- component/Recipy.jsx
+++ component/Recipy.jsx
@@ -2,14 +2,8 @@ export default function Recipy({ recipe }) {
   console.log(recipe);
   return (
     <section>
-      <div>
-        {/* <img
-          src={recipe.image}
-          className="bd-placeholder-img card-img-top"
-          width="100%"
-          height="225"
-        /> */}
-        <p>{recipe.name}</p>
+      <div class="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
+        <img src={recipe.image} alt={recipe.name} />
       </div>
     </section>
   );
