3d230f28a910f70dd215d757ce7c0b23f10f67ff
Gissell10 <gissellbustosb@gmail.com>
2023-08-21 15:03:04 +1000

    fixing recipe image

--- component/Recipy.jsx
+++ component/Recipy.jsx
@@ -2,8 +2,31 @@ export default function Recipy({ recipe }) {
   console.log(recipe);
   return (
     <section>
-      <div class="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
-        <img src={recipe.image} alt={recipe.name} />
+      <div
+        style={{
+          display: "grid",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          src={recipe.image}
+          alt={recipe.name}
+          style={{ objectFit: "cover", width: "600px", height: "600px" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "50px, 50px, 50px",
+          columnGap: "30px",
+          justifyContent: "center",
+        }}
+      >
+        <span>Ingredients</span>
+        <span>Briefly</span>
+      </div>
+      <div style={{ width: "100px" }}>
+        <p>nnnnnnn</p>
       </div>
     </section>
   );
